ff6a77d3c553b580a07671217e5507159dba152a
francis omondi <francisomondi17@gmail.com>
2022-08-29 22:32:08 +0300

    get details of a single user

--- routes/users.js
+++ routes/users.js
@@ -28,4 +28,11 @@ router.post("/", (req, res) => {
 })
 
 
+router.get("/:id", (req, res) => {
+
+    const id = req.params.id
+    const singleUser = users.find((user) => user.id === id)
+    res.send(singleUser)
+})
+
 export default router
