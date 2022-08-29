ccd0e221e7b603a63c771449bc1f501a782f11e4
francis omondi <francisomondi17@gmail.com>
2022-08-29 17:02:49 +0300

    users route /users

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import express from "express";
+import express from "express"
+import usersRoute from "./routes/users.js"
 import bodyParser from "body-parser"
 
 const app = express()
@@ -6,5 +7,11 @@ const port = 5000
 
 app.use(bodyParser.json())
 
+app.use("/users", usersRoute)
+
+app.get("/", (req, res) => {
+    res.send("this isn the home page")
+})
+
 app.listen(port, () =>
-    console.log(`Server started running on port: http://localhost${5000}`))
+    console.log(`Server started running on port: http://localhost:${5000}`))
---
+++ routes/users.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    res.send("Hello User")
+})
+
+
+export default router
